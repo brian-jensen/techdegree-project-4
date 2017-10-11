fb36e4b4ff49dea27c0745f20aafa1e749711d38
Brian Jensen <31598338+brian-jensen@users.noreply.github.com>
2017-10-10 19:45:42 -0700

    added conditional logic to display heading if no images match search

--- js/main.js
+++ js/main.js
@@ -7,8 +7,7 @@ $.extend($.fancybox.defaults, {
   idleTime: 9999999,
 });
 
-// Global variable declarations
-
+// Global variables
 var wrapper;
 var wrapperWidth;
 var ul = document.getElementById('gallery-items');
@@ -16,30 +15,38 @@ var li = ul.getElementsByTagName('li');
 window.addEventListener("resize", resizeCheck);
 
 // custom responsive javascript search filter
-  // responsive size check for justifying conent
+// responsive size check for justifying conent
 function resizeCheck() {
   wrapper = document.getElementById('wrapper');
   wrapperWidth = wrapper.clientWidth;
   // mobile view
- if (wrapperWidth <= 455) {
-   ul.style.justifyContent = 'space-between';
- }
- // tablet and larger
- else {
-   ul.style.justifyContent = 'flex-start';
- }
+  if (wrapperWidth <= 455) {
+    ul.style.justifyContent = 'space-between';
+  }
+  // tablet and larger
+  else {
+    ul.style.justifyContent = 'flex-start';
+  }
 }
-  // read and filter input search results
+// read and filter input search results
 function filterResults() {
   var input = document.getElementById('search');
   var filter = input.value.toLowerCase();
+  var negativeListItems = li.length;
+  var noMatchesText = document.getElementById('no-matches');
   // Loop through all list items, and hide those which don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName('a')[0];
+  for (var i = 0; i < li.length; i++) {
+    var a = li[i].getElementsByTagName('a')[0];
     if (a.getAttribute('data-caption').toLowerCase().indexOf(filter) > -1) {
       li[i].style.display = '';
     } else {
       li[i].style.display = 'none';
+      negativeListItems--;
+      if (negativeListItems > 0) {
+        noMatchesText.style.display = 'none';
+      } else {
+        noMatchesText.style.display = 'block';
+      }
+      }
     }
   }
-}
